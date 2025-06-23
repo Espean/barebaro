b6c0e52eca6e421d3afab1f549648260e1c678cf
espen.baro@gmail.com <espen.baro@gmail.com>
2025-06-23 14:57:39 +0200

    test workflow

---
+++ infra/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azure = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    az_api = {
+      source  = "azure/azapi"
+      version = "~> 1.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "eb-barebaro"
+    storage_account_name = "barebarosa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    subscription_id = "f275d5ca-5939-420f-bba2-e502b7489668"
+    tenant_id = "8b87af7d-8647-4dc7-8df4-5f69a2011bb5"
+    client_id = "fc516b06-7c5c-474a-9379-949fa0b54e51"
+    use_oidc = true
+  }
+}
