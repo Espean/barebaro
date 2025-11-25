c9b8f0e2c61d4450993bed2b680a19db271ee8b4
espen <espen@ess.no>
2025-11-25 14:25:07 +0100

    allow user

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,7 @@
 variable "admin_object_ids" {
 	description = "List of Entra ID object IDs (or literals) allowed to administer sounds."
 	type        = list(string)
-	default     = ["demo-user"]
+	default     = [
+		"b62b638f-5192-45c0-be88-3d25d196349c",
+	]
 }
