f7e1d5e09e0ba967665ca8916147cce01175d726
espen <espen@ess.no>
2025-11-25 14:27:34 +0100

    test new id

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,6 +2,6 @@ variable "admin_object_ids" {
 	description = "List of Entra ID object IDs (or literals) allowed to administer sounds."
 	type        = list(string)
 	default     = [
-		"b62b638f-5192-45c0-be88-3d25d196349c",
+		"3d4c4a730c1944748ee4c7fcea105330",
 	]
 }
